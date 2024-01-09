3cb49afdea38663e5fcbeaafa2cfa991ff912b24
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2024-01-09 18:53:17 +0000

    update all

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { createContext } from "react";
 import Inputform from "./components/Inputform";
-import QrCode from "./components/QrCode";
 import { useState } from "react";
 
 export const InputContext = createContext();
@@ -19,10 +18,9 @@ function App() {
   return (
     <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 h-screen pt-36 px-2">
       <div className="container mx-auto max-w-4xl bg-white rounded-md shadow">
-        <div className="md:grid md:grid-cols-3" >
-        <InputContext.Provider value={value}>
+        <div className="md:grid md:grid-cols-1" >
+          <InputContext.Provider value={value}>
             <Inputform />
-            <QrCode />
           </InputContext.Provider>
 
         </div>
--- src/components/Inputform.js
+++ src/components/Inputform.js
@@ -1,15 +1,59 @@
 import Inputcolor from "./Inputcolor"
 import Inputfield from "./Inputfield"
+import React, { useContext, useState } from "react";
+import QRCode from 'qrcode';
+import { InputContext } from "../App";
 
 const Inputform = () => {
+  const { inputValue } = useContext(InputContext);
+  const { url, color } = inputValue;
+
+  const [qr, setQr] = useState('');
+
+  const GenerateQRCode = (qrColor) => {
+    QRCode.toDataURL(url, {
+      width: 200,
+      margin: 2,
+      color: {
+        dark: qrColor || '#e2e2e2',
+        light: '#EEEEEEFF'
+      }
+    }, (err, url) => {
+      if (err) return console.error(err);
+
+      console.log(url);
+      setQr(url);
+    });
+  }
   return (
-    <div className="col-span-2 p-6 grid gap-4">
-        <Inputfield/>
-        <Inputcolor/>
-        <button className="bg-indigo-500 max-w-xs ml-auto px-4 py-2 text-white
-        rounded-sm mt-4bg-indigo-500 disabled:bg-gray-400">
-            Generate QR-Code
+    <div className="md:grid md:grid-cols-3 gap-3 p-6">
+      <div className="col-span-2">
+        <Inputfield />
+        <Inputcolor />
+
+        <button className="bg-indigo-500 max-w-xs mt-6 px-4 py-2 text-white
+          rounded-sm mt-4bg-indigo-500 disabled:bg-gray-400"
+          onClick={() => GenerateQRCode(color)}>
+          Generate QR Code
         </button>
+      </div>
+
+      <div className="bg-gray-100 rounded-r-md flex flex-col pt-2 ml-auto">
+        <div className="text-gray-500 text-center m-auto px-10"
+        >Your QR CODE HERE
+        </div>
+        {qr &&
+          <>
+            <img src={qr} alt="QR Code" />
+            <a
+              className="bg-indigo-500 max-w-xs mt-6 px-4 py-2 text-white
+              rounded-sm mt-4bg-indigo-500 disabled:bg-gray-400 text-center"
+
+              href={qr} download="qrcode.png">Download</a>
+          </>
+        }
+      </div>
+
     </div>
   )
 }
